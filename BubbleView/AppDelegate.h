810712268b4d27783a7fefeaa406cfff8e6fae0e
杨名海 <yangminghai@yangminghaideMacBook-Pro.local>
2014-08-02 21:57:09 -0700

    Initial Commit

---
+++ BubbleView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BubbleView
+//
+//  Created by 杨名海 on 14-8-2.
+//  Copyright (c) 2014年 杨名海. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
